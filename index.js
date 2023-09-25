0e4be56ce22f3451acf74dfae45be06b01bb8d20
Talles Alves <talves2503@gmail.com>
2023-09-25 19:14:35 -0300

    basic js function

---
+++ index.js
@@ -0,0 +1,37 @@
+function rmButton(id) {
+    const remove = document.createElement('button');
+    const icon = document.createElement('img');
+
+    icon.src = './assets/remove';
+    remove.id = `rm-${id}`;
+
+    remove.append(icon);
+    return remove;
+}
+
+function createTd(value) {
+    const td = document.createElement('td');
+    td.innerText = value;
+    return td;
+}
+
+function renderEntry(entryData) {
+    const row = document.createElement('tr');
+    row.id = `entry-${entryData.id}`;
+
+    const rowContent = [entryData.id, entryData.name, entryData.value, entryData.description];
+    rowContent.forEach(item => {
+        row.append(createTd(item));
+    });
+
+    const body = document.querySelector('#entries');
+    body.append(row);
+}
+
+async function getEntries() {
+    const entries = await fetch('http://localhost:3000/entries').then(res => res.json());
+    // console.log(entries);
+    renderEntry(entries);
+}
+
+getEntries();
