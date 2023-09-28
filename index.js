1c86674142cbba155ad25108cda1e27e87299422
Talles Alves <talves2503@gmail.com>
2023-09-28 13:41:24 -0300

    PUT method

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const name = document.querySelector('#name');
+const value = document.querySelector('#value');
+const description = document.querySelector('#description');
+
 function createTd(value) {
     const td = document.createElement('td');
     td.innerText = value;
@@ -40,9 +44,9 @@ form.addEventListener('submit', async (ev) => {
     ev.preventDefault();
 
     const transactionData = {
-        name: document.querySelector('#name').value,
-        value: document.querySelector('#value').value,
-        description: document.querySelector('#description').value,
+        name: name.value,
+        value: value.value,
+        description: description.value,
     };
 
     const response = await fetch('http://localhost:3000/entries', {
@@ -67,5 +71,36 @@ del.addEventListener('click', async () => {
     });
 
     getEntries();
+    location.reload(true);
+})
+
+const edit = document.querySelector('#editTransaction');
+edit.addEventListener('click', async () => {
+    const id = prompt("Digite o id da transação que deseja editar");
+
+    const entry = await fetch('http://localhost:3000/entries/'+id).then(res => res.json());
+
+    let data = {
+        name: entry.name,
+        value: entry.value,
+        description: entry.description
+    }
+
+    const newName = prompt(`O nome atual é: "${data.name}", digite o novo nome:`);
+    const newValue = prompt(`O valor atual é: "${data.value}", digite o novo valor:`);
+    const newDescription =  prompt(`A descrição atual é: "${data.description}", digite a nova descrição:`);
+
+    data.name = newName;
+    data.value = newValue;
+    data.description = newDescription;
+
+    await fetch('http://localhost:3000/entries/'+id, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+    
     location.reload(true);
 })
